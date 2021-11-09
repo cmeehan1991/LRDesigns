0c7387bc445eae95676fe08362caa0efaa527b82
Connor Meehan <cmeehan@elon.edu>
2021-11-09 15:57:31 +0000

    Switching to webpack

--- includes/functions/functions-assets.php
+++ includes/functions/functions-assets.php
@@ -2,7 +2,7 @@
 
 function lrdesigns_custom_styles(){
     //wp_enqueue_style('mapbox', 'https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.css', array(), '2.2.1');
-    wp_enqueue_style('allstyles', BLOG_URI.'/assets/styles/dist/allstyles.css', array(), rand());
+    wp_enqueue_style('allstyles', BLOG_URI.'/assets/styles/dist/app.css', array(), rand());
 }
 add_action('wp_enqueue_scripts','lrdesigns_custom_styles');
 
@@ -10,7 +10,7 @@ add_action('wp_enqueue_scripts','lrdesigns_custom_styles');
 // Javascript files - front end
 add_action('wp_enqueue_scripts','lrdesigns_scripts');
 function lrdesigns_scripts(){
-    wp_register_script('allscripts', BLOG_URI . '/assets/scripts/js/dist/allscripts.min.js', array(), rand(), false);
+    wp_register_script('allscripts', BLOG_URI . '/assets/scripts/js/dist/app.js', array(), rand(), false);
     wp_localize_script('allscripts', 'rest_api', array(
 	    'base'			=> get_rest_url(),
 	    'get_vendors'	=> get_rest_url(get_current_blog_id(), 'lr/v1/get-vendors'),
