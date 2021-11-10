55aee0f121b3d937767be1044368b1a3423a8919
Connor Meehan <cmeehan@elon.edu>
2021-11-10 19:23:54 +0000

    Updating styles and fixing icons in user dashboard

--- assets/scripts/js/app.js
+++ assets/scripts/js/app.js
@@ -2,6 +2,7 @@ import 'jquery';
 import '@popperjs/core';
 import 'bootstrap';
 import 'list.js';
+import '@fortawesome/fontawesome-free';
 
 import './src/vendors.js';
 import './src/woocommerce.js';
